17d249ea05ed66e301ade6778c72181bb33e2109
Esterling Accime <accimeesterlin@yahoo.com>
2023-04-03 01:43:23 -0700

    adding lock icons

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ log("Loading app.js");
 const fullNameElement = document.querySelector('#fullName');
 const expirationElement = document.querySelector('#expiration');
 const cardNumberElement = document.querySelector('#cardNumber');
+const lockIconElement = document.querySelector('.fa-lock');
 const cvvElement = document.getElementById('cvv');
 
 const params = new URLSearchParams(location.search);
@@ -26,6 +27,14 @@ const status = getParameterByName("status");
 const bearerToken = `Bearer ${token}`;
 
 
+const fractionize = (number) => {
+    let numerator = number?.slice(0, 2);
+    let denominator = number?.slice(2, 4);
+
+    return `${numerator}/${denominator}`;
+}
+
+
 if (status === "pending") {
     fullNameElement.innerHTML = `${fullName}`;
     cardNumberElement.innerHTML = `**** **** **** ****`;
@@ -34,25 +43,70 @@ if (status === "pending") {
     cvvElement.innerHTML = `***`;
 }
 
-const onSucces = function() {
+const onSuccess = function () {
     log("Loading success");
     const number = window.OpcUxSecureClient.span('cardNumber', cardNumber);
     number.mount(cardNumberElement);
 
-    
+
     var cvv = window.OpcUxSecureClient.span('cvv', cvc);
     cvv.mount(document.getElementById('cvv'));
 
     fullNameElement.innerHTML = `${fullName}`;
-    expirationElement.innerHTML = `${expiration}`;
+    expirationElement.innerHTML = `${fractionize(expiration)}`;
+
+    secureIframe = document.querySelector('iframe');
 }
 
-const onError = function(e) {
+const onError = function (e) {
     console.error('Decode Failed: ' + e);
 }
 
-// Initialize the client
-if (window.OpcUxSecureClient && status === "success") {
-    window.OpcUxSecureClient.init(uiKey);
-    window.OpcUxSecureClient.associate(bearerToken, onSucces, onError);
+const handleSensitiveData = () => {
+    if (status === "pending") {
+        lockIconElement.style.display = "none";
+    }
+    cardNumberElement.innerHTML = `**** **** **** ****`;
+    cvvElement.innerHTML = `***`;
+    expirationElement.innerHTML = `**/**`;
+    fullNameElement.innerHTML = `${fullName}`;
 }
+
+handleSensitiveData();
+
+const initApp = () => {
+    try {
+        // Initialize the client
+        if (window.OpcUxSecureClient && status === "success") {
+            window.OpcUxSecureClient.init(uiKey);
+            window.OpcUxSecureClient.associate(bearerToken, () => { }, onError);
+        }
+    } catch (error) {
+        console.log("Error: ", error);
+    }
+}
+
+initApp();
+
+document.addEventListener('click', function (e) {
+    const target = e.target;
+
+    if (target.matches('.fa-lock-open') || target.matches('.fa-lock')) {
+        const className = target?.getAttribute('class');
+
+        if (className?.includes('fa-lock-open')) {
+            // Hide sensitive data
+            target.classList.remove('fa-lock-open');
+            target.classList.add('fa-lock');
+            handleSensitiveData();
+
+        } else {
+            // Show sensitive data
+            target.classList.remove('fa-lock');
+            target.classList.add('fa-lock-open');
+            onSuccess();
+        }
+    }
+})
+
+
